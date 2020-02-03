df50933931ca47d073de51ef880d8810daa1fb14
NadineWaelti <nadine.waelti@students.hevs.ch>
2020-02-03 23:04:24 +0100

    aspect visuel page contact

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100, required=True)
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    subject = forms.CharField(label='Sujet', max_length=200, required=True)
+    name = forms.CharField(label='Votre nom',max_length=200, required=True)
+    email = forms.EmailField(label='Votre email',max_length=200, required=True)
+    message = forms.CharField(label='Message',widget=forms.Textarea, required=True)
