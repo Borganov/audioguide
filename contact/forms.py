fc42aa54a8723bb3bd8c43b6dce2b3cbd1745336
NadineWaelti <nadine.waelti@students.hevs.ch>
2020-02-05 21:36:13 +0100

    i18n et bootstrap en local

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(label='Sujet', max_length=50, required=True)
-    name = forms.CharField(label='Votre nom',max_length=50, required=True)
-    email = forms.EmailField(label='Votre email',max_length=100, required=True)
-    message = forms.CharField(label='Message',widget=forms.Textarea, required=True)
+    name = forms.CharField(label='Votre nom', max_length=50, required=True)
+    email = forms.EmailField(label='Votre email', max_length=200, required=True)
+    message = forms.CharField(label='Message', widget=forms.Textarea, required=True)
