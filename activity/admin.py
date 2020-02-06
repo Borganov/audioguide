3387530e0a94cc40f344225c513c6641d528acd7
NadineWaelti <nadine.waelti@students.hevs.ch>
2020-02-06 22:20:09 +0100

    format

--- activity/admin.py
+++ activity/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from activity.models import Activity
 
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('description', 'img', 'state', 'date', 'lang',)
+    list_filter = ('description', 'img', 'state', 'date', 'lang',)
+    search_fields = ('description', 'img', 'state', 'date', 'lang',)
 
 
 admin.site.register(Activity, ActivityAdmin)
--- position/admin.py
+++ position/admin.py
@@ -6,9 +6,14 @@ from .models import Position, PositionItem
 
 class PositionAdmin(admin.ModelAdmin):
     list_display = ('order', 'name', 'img',)
+    list_filter = ('order', 'name', 'img',)
+    search_fields = ('order', 'name', 'img',)
+
 
 class PositionItemAdmin(admin.ModelAdmin):
     list_display = ('titre', 'description', 'lang', 'audio', 'position',)
+    list_filter = ('titre', 'description', 'lang', 'audio', 'position',)
+    search_fields = ('titre', 'description', 'lang', 'audio', 'position',)
 
 
 admin.site.register(Position, PositionAdmin)
