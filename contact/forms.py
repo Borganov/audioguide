fca0f2b2480645547d823444fbfc9d4cb5dce9b6
Gab <thinkys@live.com>
2020-02-11 17:00:18 +0100

    Add geo admin map and reconfig form

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(label='Subject', max_length=50, required=True)
-    name = forms.CharField(label='Name', max_length=50, required=True)
-    email = forms.EmailField(label='Email', max_length=50, required=True)
-    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'cols':24}), required=True)
+    subject = forms.CharField(label='Subject', max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'contactForm_field'}))
+    name = forms.CharField(label='Name', max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'contactForm_field'}))
+    email = forms.EmailField(label='Email', max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'contactForm_field'}))
+    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'cols':24,'class': 'contactForm_field'}), required=True)
