8518fa13dbc538e643a272a80151d4dcfadc18bf
Borganov <berclazalexandre@gmail.com>
2020-02-13 14:18:22 +0100

    Remove unused code

--- position/views.py
+++ position/views.py
@@ -69,27 +69,3 @@ class Detail(TemplateView):
 
 
         return context
-
-
-class DetailByIdPositionItem(TemplateView):
-    #set html file to use
-    template_name = 'position/positionItem.html'
-
-    def get_context_data(self, **kwargs):
-        lang = Language.objects.get(abreviation=self.request.LANGUAGE_CODE)
-
-        #Traitement du formulaire
-        context = super().get_context_data(**kwargs)
-
-        #Get indicator data
-        idPosition = kwargs.get('id')
-        positionItem = PositionItem.objects.filter(position__id=idPosition, lang=lang.id)
-        position = Position.objects.get(id = idPosition)
-
-        context = {
-            'positionItem': positionItem[0],
-            'position' : position.__dict__
-            }
-
-
-        return context
