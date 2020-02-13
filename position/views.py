e401767e53877be4b6cf6f83c3a099e2add1455f
Borganov <berclazalexandre@gmail.com>
2020-02-13 14:15:43 +0100

    Correct Position and position item view

--- position/views.py
+++ position/views.py
@@ -30,6 +30,61 @@ class Detail(TemplateView):
         idPosition = kwargs.get('id')
         positionItem = PositionItem.objects.filter(position__id=idPosition, lang=lang.id)
         position = Position.objects.get(id = idPosition)
+        positions = Position.objects.all()
+
+        current_index = 0
+        previousId = 0
+        nextId = 0
+        #Set Index
+        for i,  p in enumerate(positions):
+            if str(p.id) == str(idPosition) :
+                current_index = i
+
+        #Set hasPrevious
+        if current_index > 0:
+            hasPrevious = True
+            previousId = positions[current_index - 1].id
+        else :
+            hasPrevious = False
+            previousId = -1
+
+        #Set hasNext
+        if current_index < len(positions)-1:
+            hasNext = True
+            nextId = positions[current_index + 1].id
+        else :
+            hasNext = False
+
+
+        context = {
+            'positionItem': positionItem[0],
+            'position' : position.__dict__,
+            'nextId' : nextId,
+            'previousId' : previousId,
+            'hasNext' : hasNext,
+            'hasPrevious' : hasPrevious,
+            }
+
+        print(context)
+
+
+        return context
+
+
+class DetailByIdPositionItem(TemplateView):
+    #set html file to use
+    template_name = 'position/positionItem.html'
+
+    def get_context_data(self, **kwargs):
+        lang = Language.objects.get(abreviation=self.request.LANGUAGE_CODE)
+
+        #Traitement du formulaire
+        context = super().get_context_data(**kwargs)
+
+        #Get indicator data
+        idPosition = kwargs.get('id')
+        positionItem = PositionItem.objects.filter(position__id=idPosition, lang=lang.id)
+        position = Position.objects.get(id = idPosition)
 
         context = {
             'positionItem': positionItem[0],
