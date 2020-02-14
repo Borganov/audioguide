62884d3df56929b6f3291a5abbf5ea42b63c7db0
Borganov <berclazalexandre@gmail.com>
2020-02-14 10:07:20 +0100

    Delete don pages and views

--- don/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from activity.models import Language
-
-
-def don(request):
-    lang = Language.objects.get(abreviation=request.LANGUAGE_CODE)
-    return render(request, 'don/don.html')
